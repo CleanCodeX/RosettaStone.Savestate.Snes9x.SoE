c1eda2bf4b7bd5c36430bb5f3cfb051ec50aaced
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2021-01-13 03:25:47 +0100

    initial commit

---
+++ Extensions/StreamExtensions.Sram.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using RosettaStone.Savestate.Snes9x.Extensions;
+using RosettaStone.Savestate.Snes9x.Helpers;
+using RosettaStone.Savestate.Snes9x.SoE.Helpers;
+
+namespace RosettaStone.Savestate.Snes9x.SoE.Extensions
+{
+	public static class StreamExtensions
+    {
+	    public static Stream? GetSramFromSavestate(this Stream source) => SavestateWramExtractor.ExtractSram(SavestateReader.Load(source).RAM.Data).ToStreamIfNotNull();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using RosettaStone.Savestate.Snes9x.Extensions;
+using RosettaStone.Savestate.Snes9x.Helpers;
+using RosettaStone.Savestate.Snes9x.SoE.Helpers;
+using RosettaStone.Sram.SoE;
+using RosettaStone.Sram.SoE.Enums;
+
+var wramBytes = SavestateReader.Load(args[0]).RAM.Data;
+var sramStream = SavestateWramExtractor.ExtractSram(wramBytes).ToStream()!;
+
+Debug.Assert(false);
+
+var sramFile = new SramFileSoE(sramStream, GameRegion.EnglishNtsc);
+var saveSlot = sramFile.GetSaveSlot(0);
+
+Debug.Print(saveSlot.BoyCurrentHp.ToString());
+
+
